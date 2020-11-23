7d151c6c0e4953b5eed9eb6d902219754085a892
carltgoss <ctgoss1840@gmail.com>
2020-11-23 16:39:02 -0500

    FirstCommitforBooks

---
+++ CompletelyBookedBlue/CompletelyBooked.Models/BookCreate.cs
@@ -0,0 +1,30 @@
+﻿using CompletelyBooked.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompletelyBooked.Models
+{
+    public class BookCreate
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public Group Group { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        [Required]
+        [MaxLength(5000, ErrorMessage = "Description needs to be less than 5000 characters.")]
+        public string Description { get; set; }
+        public bool IsBestSeller { get; set; }
+        public string ISBN { get; set; }
+
+    }
+}
