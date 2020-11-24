4fa282f812a89366ec990a717d37b17222fa6ad0
sedutiel <samantha.dutiel@gmail.com>
2020-11-24 11:22:20 -0500

    author list item update

--- CompletelyBookedBlue/CompletelyBooked.Models/AuthorListItem.cs
+++ CompletelyBookedBlue/CompletelyBooked.Models/AuthorListItem.cs
@@ -7,11 +7,7 @@ namespace CompletelyBooked.WebAPI.Models
 {
     public class AuthorListItem
     {
-        public int AuthorId { get; set; }
         public int AuthorName { get; set; }
-        public string AuthorBirthday { get; set; }
-        public string AuthorBirthplace { get; set; }
-        public string AuthorAbout{ get; set; }
-      
+     
     }
 }
