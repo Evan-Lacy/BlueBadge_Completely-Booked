56b98e90a074eedb589b64afb339f09049a7bf49
carltgoss <ctgoss1840@gmail.com>
2020-11-24 09:59:22 -0500

    removed a comment

--- CompletelyBookedBlue/CompletelyBooked.Data/IdentityModels.cs
+++ CompletelyBookedBlue/CompletelyBooked.Data/IdentityModels.cs
@@ -33,9 +33,9 @@ namespace CompletelyBooked.Data
             return new ApplicationDbContext();
         }
 
-        //public DbSet<Author> Authors { get; set; }
+        public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
-        //public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
         //public DbSet<Review> Reviews { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
