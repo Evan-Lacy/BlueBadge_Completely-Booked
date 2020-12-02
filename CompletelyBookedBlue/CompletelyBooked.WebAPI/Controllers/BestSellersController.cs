0c0d319289695b1ce4caba05d4bf33f4b16aa997
sedutiel <samantha.dutiel@gmail.com>
2020-12-02 15:54:25 -0500

    comments updated; book controller updated

--- CompletelyBookedBlue/CompletelyBooked.WebAPI/Controllers/BestSellersController.cs
+++ CompletelyBookedBlue/CompletelyBooked.WebAPI/Controllers/BestSellersController.cs
@@ -10,6 +10,7 @@ namespace CompletelyBooked.WebAPI.Controllers
 {
     public class BestSellersController : ApiController
     {
+        
         private BookService CreateBookService()
         {
             // var userId = Guid.Parse(User.Identity.GetUserId());
@@ -17,18 +18,29 @@ namespace CompletelyBooked.WebAPI.Controllers
             return bookService;
         }
 
+      
         private AuthorService CreateAuthorService()
         {
             var authorService = new AuthorService();
             return authorService;
         }
 
+        /// <summary>
+        /// This is to get a list of all Best Sellers 
+        /// </summary>
+        /// <returns></returns>
         public IHttpActionResult GetAllBestSellers()
         {
             BookService bookService = CreateBookService();
             var books = bookService.GetBestSellers();
             return Ok(books);
         }
+
+        /// <summary>
+        /// This is to get a list of Best Sellers by Author ID
+        /// </summary>
+        /// <param name="id">This is to get a Best Seller List within the Completely Booked Database using the Author ID</param>
+        /// <returns></returns>
 
         public IHttpActionResult Get(int id)
         {
@@ -37,6 +49,11 @@ namespace CompletelyBooked.WebAPI.Controllers
             return Ok(author);
         }
 
+        /// <summary>
+        /// This is to get a list of Best Sellers by Author Name
+        /// </summary>
+        /// <param name="name">This is to get a Best Seller List within the Completely Booked Database using the Author Name</param>
+        /// <returns></returns>
         public IHttpActionResult Get(string name)
         {
             AuthorService authorService = CreateAuthorService();
